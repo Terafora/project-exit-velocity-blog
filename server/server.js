3c9adeed312d6b2203edc7ac73591ebbdd63b436
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-10-14 16:08:22 +0100

    Setup test route and default port

--- server/server.js
+++ server/server.js
@@ -11,3 +11,14 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error("Uh-oh, there was an issue connecting to MongoDB: ", err));
 
+// Routes
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Everything is working!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
