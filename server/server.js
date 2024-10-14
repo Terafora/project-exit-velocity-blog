a1695b3ae85f0cf1391077e0cbcfe9e8f77a1729
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-10-14 16:33:58 +0100

    Add console log for debugging purposes

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error("Uh-oh, there was an issue connecting to MongoDB: ", err));
 
+    console.log('Mongo URI:', process.env.MONGO_URI);
+
+
 // Routes
 
 // Test route
