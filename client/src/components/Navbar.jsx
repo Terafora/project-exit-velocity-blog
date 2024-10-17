108f70e918b87f4b6926b147cbcd45ed778b819b
terafora <charlie.stone649@gmail.com>
2024-10-17 16:58:20 +0100

    Update Navbar components layout and stylings

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import '../stylings/Navbar.scss'; 
+import '../stylings/Navbar.scss';
 
 const Navbar = () => {
     return (
         <div className="navbar-wrapper">
             {/* First Shadow div */}
             <div className="navbar-shadow"></div>
-            {/* Second Shadow div */}
-            <div className="navbar-shadow-top"></div>
-            {/* Actual navbar */}
-            <nav className="navbar">
-                <ul className="navbar-list">
-                    {/* Main Navbar Items */}
-                    <li className="navbar-item"><a href="#home">Home</a></li>
-                    <li className="navbar-item"><a href="#about">About</a></li>
-                    <li className="navbar-item"><a href="#blog">Blog</a></li>
-                    <li className="navbar-item"><a href="#contact">Contact</a></li>
+            <div className="navbar-shadow navbar-shadow-two"></div>
 
-                    {/* Dropdown Menu */}
-                    <li className="navbar-item dropdown">
-                        <a href="#languages" className="dropdown-toggle">Languages</a>
-                        <ul className="dropdown-menu">
-                            <li className="dropdown-item"><a href="#english">English</a></li>
-                            <li className="dropdown-item"><a href="#french">French</a></li>
-                            <li className="dropdown-item"><a href="#japanese">Japanese</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
+            {/* Actual navbar (moving, but without items) */}
+            <nav className="navbar"></nav>
+            <div className="navbar navbar-two"></div>
+            {/* Navbar items moved outside the navbar */}
+            <ul className="navbar-list">
+                <li className="navbar-item"><a href="#home">Home</a></li>
+                <li className="navbar-item"><a href="#about">About</a></li>
+                <li className="navbar-item"><a href="#blog">Blog</a></li>
+                <li className="navbar-item"><a href="#contact">Contact</a></li>
+
+                {/* Dropdown Menu */}
+                <li className="navbar-item dropdown">
+                    <a href="#languages" className="dropdown-toggle">Languages</a>
+                    <ul className="dropdown-menu">
+                        <li className="dropdown-item"><a href="#english">English</a></li>
+                        <li className="dropdown-item"><a href="#french">French</a></li>
+                        <li className="dropdown-item"><a href="#japanese">Japanese</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     );
 };
