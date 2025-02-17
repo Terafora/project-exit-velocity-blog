6e8ddc0d71ebb8b6d5f0893d16f39265e4a9d7a2
terafora <charlie.stone649@gmail.com>
2025-02-17 15:47:31 +0000

    Remove unused SVG assets and add Spanish language support to Post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -5,13 +5,15 @@ const postSchema = new mongoose.Schema({
         en: { type: String, required: true },
         fr: { type: String, required: true },
         ja: { type: String, required: true },
-        eo: { type: String, required: true }
+        eo: { type: String, required: true },
+        es: { type: String, required: true }
     },
     content: {
         en: { type: String, required: true },
         fr: { type: String, required: true },
         ja: { type: String, required: true },
-        eo: { type: String, required: true }
+        eo: { type: String, required: true },
+        es: { type: String, required: true }
     },
     author: { type: String, required: true },
     tags: [{ type: String }],
